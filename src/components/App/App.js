cff404d8644f4608adb05c613c2b8b89aec4e71f
Matt Fowler <mbfowler@gmail.com>
2023-07-09 18:53:51 -0400

    Fetch movie data from live api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import movieData from './movieData'
 import Movie from '../Movie/Movie';
@@ -7,7 +7,16 @@ import Card from '../Card/Card'
 function App() {
   const [isClicked, setIsClicked] = useState(false)
   const [clickedMovie, setClickedMovie] = useState({})
-  const cards = movieData.movies.map(movie => <Card setClickedMovie={setClickedMovie} setIsClicked={setIsClicked} key={movie.id} movieData={movie}/>)
+  const [movieData, setMovieData] = useState([])
+
+  const cards = movieData.map(movie => <Card setClickedMovie={setClickedMovie} setIsClicked={setIsClicked} key={movie.id} movieData={movie}/>)
+
+  useEffect(() => {
+    fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+      .then(res => res.json())
+      .then(data => setMovieData(data.movies))
+      .catch(err => console.log(err))
+  }, [])
 
   return (
     <main>
